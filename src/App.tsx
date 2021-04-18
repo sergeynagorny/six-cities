74fcf7c7bc85aa8a782f108d7e7c7bbe828f10e2
sergey nagorny <sergey.s.nagorny@gmail.com>
2021-04-19 01:06:44 +0300

    feat(app): isAuthenticated render routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,15 @@ import React from 'react';
 import { BrowserRouter as Router } from "react-router-dom";
 import { routes } from './pages';
 import { renderRoutes } from '@lib/render-routes/index';
+import { useStore } from 'effector-react';
+import { $isAuthenticated } from '@features/common/model/session';
 
 function App() {
-  const isAuthenticated = false;
-  
-  return <Router>{renderRoutes(routes, isAuthenticated)}</Router>;
+  const isAuthenticated = useStore($isAuthenticated);
+  console.log("Render App, status: ", isAuthenticated);
+  return (
+    <Router>{isAuthenticated !== null ? renderRoutes(routes, isAuthenticated) : null}</Router>
+  );
 }
 
 export default App;
