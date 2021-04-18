b78d4d60bf42c6b95e689c9665fed73d29c05774
sergey nagorny <sergey.s.nagorny@gmail.com>
2021-04-19 00:59:49 +0300

    feat(config): update aslis paths

--- craco.config.js
+++ craco.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 module.exports = {
   webpack: {
     alias: {
+      "@api": path.resolve(__dirname, "src/api"),
       "@features": path.resolve(__dirname, "src/features/"),
       "@images": path.resolve(__dirname, "src/assets/img/"),
       "@mock": path.resolve(__dirname, "src/mock"),
